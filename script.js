8e272f151533f6f977378538d316dc0ff60b7842
Jardel Almeida <jardelalmeida802@gmail.com>
2025-04-20 14:41:02 -0300

    Add new page or updates

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function salvarnota() {
   let notasalva = document.getElementById("blocodenotas").value;
-  let palavras = notasalva.split(/\s+/); // Divide o texto em palavras
-  let numPalavras = palavras.length; // Contagem de palavras
+  let palavras = notasalva.split(/\s+/);
+  let numPalavras = palavras.length;
   if (numPalavras > 83) {
     document.getElementById("erros").innerHTML =
       "limite de palavras excedeu! digite menos que 83 palavras.";
     return;
   } else if (notasalva.trim() === "") {
-    document.getElementById("erros").innerHTML = "digite algo!";
+    document.getElementById("erros").innerHTML = "<BR>Digite algo!";
   } else {
     const novadiv = document.createElement("div");
     novadiv.textContent = notasalva;
